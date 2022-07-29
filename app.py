3d8f1b452262e6dd141236404e80dddd959d88af
Kellie <kellie.manasco@gmail.com>
2022-07-29 10:36:38 -0400

    changing session to permanent

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ class List(db.Model):
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
